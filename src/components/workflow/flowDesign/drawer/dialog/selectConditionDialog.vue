cab5956af160a337319a02ea5b4b6e686ee6fc3e
lidonghui <574427343@qq.com>
2026-05-28 22:18:42 +0800

    RF:系统重构，破旧立新，质效跃升，焕然新生

--- src/components/workflow/flowDesign/drawer/dialog/selectConditionDialog.vue
+++ src/components/workflow/flowDesign/drawer/dialog/selectConditionDialog.vue
@@ -81,7 +81,7 @@ const loadLFFormCondition = computed(() => {
         zdy2: "",
         opt2: "<",
         columnDbName: item.id,
-        columnType: matchDatabasetype.get(item.type) || "String",
+        columnDbType: matchDatabasetype.get(item.type) || "String",
         fixedDownBoxValue: JSON.stringify(optionGroup)
       };
 
